c5420015217807e8934b65323e8df19063aec182
Zacarías S. Quispe <80052640+zaca343@users.noreply.github.com>
2022-02-15 18:47:28 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import AppRouter from './Componentes/AppRouter/AppRouter'
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
